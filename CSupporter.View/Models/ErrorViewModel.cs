69a4fc9dcd0b51f32bba9a88b47b5d0d78c75086
kicbar <bartlomiejkicior@gmail.com>
2021-11-27 08:41:00 +0100

    add frontend project

---
+++ CSupporter.View/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSupporter.View.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
