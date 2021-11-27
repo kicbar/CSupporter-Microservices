331f04d3bb3d97bb80515ff51dfa3830a47c12db
kicbar <bartlomiejkicior@gmail.com>
2021-11-27 09:09:53 +0100

    add default class for contractor

---
+++ CSupporter.Services.Contractors/Models/Dtos/ContractorDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSupporter.Services.Contractors.Models.Dtos
+{
+    public class ContractorDto
+    {
+    }
+}
---
+++ CSupporter.Services.Contractors/Repositories/IRepositories/IContractorRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSupporter.Services.Contractors.Repositories.IRepositories
+{
+    public interface IContractorRepository
+    {
+    }
+}
