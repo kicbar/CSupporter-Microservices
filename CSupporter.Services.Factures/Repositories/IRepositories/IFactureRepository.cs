39c9e890479b1a1809af0804e85b3d9af5bef863
kicbar <bartlomiejkicior@gmail.com>
2021-11-28 19:37:19 +0100

    add template for factures

---
+++ CSupporter.Services.Factures/Repositories/IRepositories/IFactureRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSupporter.Services.Factures.Repositories.IRepositories
+{
+    public interface IFactureRepository
+    {
+    }
+}
