bff5010efd80b39c72db387418ce44bd3e69ecad
kicbar <bartlomiejkicior@gmail.com>
2021-11-28 12:47:16 +0100

    add db configuration, context and seed example data

--- CSupporter.Services.Contractors/Data/DbContexts/ContractorDbContext.cs
+++ CSupporter.Services.Contractors/Data/DbContexts/ContractorDbContext.cs
@@ -1,11 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CSupporter.Services.Contractors.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSupporter.Services.Contractors.Data.DbContexts
 {
-    public class ContractorDbContext
+    public class ContractorDbContext : DbContext
     {
+        public ContractorDbContext(DbContextOptions<ContractorDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contractor> Contractors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Contractor>().HasData(new Contractor
+            {
+                ContractorId = 1,
+                FirstName = "Richard",
+                LastName = "Nowak",
+                Address = "Warszawa, ul. Przedszkolna 15",
+                CompanyName = "GM Prime RN",
+                CompanyDetails = "--",
+                NIP = "912-111-09-10"
+            });
+            modelBuilder.Entity<Contractor>().HasData(new Contractor
+            {
+                ContractorId = 2,
+                FirstName = "Adrian",
+                LastName = "Kowalski",
+                Address = "Gdański, ul. Mariacka 8",
+                CompanyName = "FUH Adrian",
+                CompanyDetails = "--",
+                NIP = "805-111-09-10"
+            });
+            modelBuilder.Entity<Contractor>().HasData(new Contractor
+            {
+                ContractorId = 3,
+                FirstName = "Roman",
+                LastName = "Romanowicz",
+                Address = "Wrocław, ul. Żeromskiego 10",
+                CompanyName = "KOMFORT",
+                CompanyDetails = "--",
+                NIP = "712-934-35-23"
+            });
+        }
     }
 }
--- CSupporter.Services.Contractors/Startup.cs
+++ CSupporter.Services.Contractors/Startup.cs
@@ -1,16 +1,14 @@
+using AutoMapper;
+using CSupporter.Services.Contractors.Data.DbContexts;
+using CSupporter.Services.Contractors.Mappings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CSupporter.Services.Contractors
 {
@@ -23,9 +21,14 @@ namespace CSupporter.Services.Contractors
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ContractorDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -34,7 +37,6 @@ namespace CSupporter.Services.Contractors
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
