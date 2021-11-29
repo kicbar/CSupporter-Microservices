08520095fbd5fc98e2e552f668d8ea81e0f1ce06
kicbar <bartlomiejkicior@gmail.com>
2021-11-29 15:29:03 +0100

    implement base srvice sender

--- CSupporter.Services.Factures/Services/BaseService.cs
+++ CSupporter.Services.Factures/Services/BaseService.cs
@@ -1,9 +1,12 @@
 ﻿using CSupporter.Services.Factures.Models;
 using CSupporter.Services.Factures.Models.Dtos;
 using CSupporter.Services.Factures.Services.IServices;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CSupporter.Services.Factures.Services
@@ -11,15 +14,69 @@ namespace CSupporter.Services.Factures.Services
     public class BaseService : IBaseService
     {
         public ResponseDto responseModel { get; set; }
+        public IHttpClientFactory httpClient;
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new ResponseDto();
+            this.httpClient = httpClient;
+        }
+
+        public async Task<T> SendAsync<T>(RequestAPI requestAPI)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("CSupprterAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(requestAPI.Url);
+                client.DefaultRequestHeaders.Clear();
+
+                if (requestAPI.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestAPI.Data), Encoding.UTF8, "application/json");
+                }
+                HttpResponseMessage apiResponse = null;
+
+                switch (requestAPI.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+                return apiResponseDto;
+            }
+            catch (Exception exc)
+            {
+                var dto = new ResponseDto
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessage = new List<string> { Convert.ToString(exc.Message) },
+                    IsSuccess = false,
+                };
+
+                var res = JsonConvert.SerializeObject(dto);
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(res);
+                return apiResponseDto;
+            }
+        }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> SendAsync<T>(RequestAPI requestAPI)
-        {
-            throw new NotImplementedException();
+            GC.SuppressFinalize(true);
         }
     }
 }
