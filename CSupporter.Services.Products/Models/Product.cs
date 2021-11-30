02483eca2f9e5915bc0794c8fa11c0f4b45acfaa
kicbar <bartlomiejkicior@gmail.com>
2021-11-30 22:55:47 +0100

    add template for products and income project

---
+++ CSupporter.Services.Products/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace CSupporter.Services.Products.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public string ProductDetails { get; set; }
+    }
+}
---
+++ CSupporter.Services.Products/Models/Warehouse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSupporter.Services.Products.Models
+{
+    public class Warehouse
+    {
+        public int WarehouseId { get; set; }
+        public int ProductId { get; set; }
+        public int Amount { get; set; }
+        public DateTime UpdateDate { get; set; } = DateTime.Now;
+    }
+}
