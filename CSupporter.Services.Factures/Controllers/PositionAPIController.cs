8e56699abcac80160f2020c00ed33cea09bf6b74
kicbar <bartlomiejkicior@gmail.com>
2021-11-30 21:35:15 +0100

    add controller for positions

---
+++ CSupporter.Services.Factures/Controllers/PositionAPIController.cs
@@ -0,0 +1,27 @@
+﻿using CSupporter.Services.Factures.Models;
+using CSupporter.Services.Factures.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CSupporter.Services.Factures.Controllers
+{
+    [ApiController]
+    [Route("api/position")]
+    public class PositionAPIController : Controller
+    {
+        private readonly IPositionService _positionService;
+
+        public PositionAPIController(IPositionService positionService)
+        {
+            _positionService = positionService;
+        }
+
+        [HttpGet]
+        [Route("factureId")]
+        [ActionName("GetAllPositionsForFacture")]
+        public ActionResult<List<Position>> GetAllPositionsForFacture(int factureId)
+        {
+            return Ok(_positionService.GetAllPositionsForFacture(factureId));
+        }
+    }
+}
