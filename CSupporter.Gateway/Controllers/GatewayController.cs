03d19d428b665fd4b14c612459130e744f2509f0
kicbar <bartlomiejkicior@gmail.com>
2021-12-08 13:43:29 +0100

    add default controller

---
+++ CSupporter.Gateway/Controllers/GatewayController.cs
@@ -0,0 +1,23 @@
+﻿using CSupporter.Gateway.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSupporter.Gateway.Controllers
+{
+    [ApiController]
+    [Route("api/gateway")]
+    public class GatewayController : Controller
+    {
+        public GatewayController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("/api/gateway/facture")]
+        [ActionName("GetFactureWithDetails")]
+        public ActionResult<FactureDto> GetFactureWithDetails()
+        {
+            return Ok("Return facture dto");
+        }
+    }
+}
