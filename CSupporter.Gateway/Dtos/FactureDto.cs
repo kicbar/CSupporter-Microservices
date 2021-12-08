f9d7aad930b9fff306076af2c437fa98a872ac41
kicbar <bartlomiejkicior@gmail.com>
2021-12-08 13:29:53 +0100

    add models for gateway

---
+++ CSupporter.Gateway/Dtos/FactureDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSupporter.Gateway.Dtos
+{
+    public class FactureDto
+    {
+        public int FactureId { get; set; }
+        public string FactureNo { get; set; }
+        public string FactureType { get; set; }
+        public DateTime FactureDate { get; set; }
+        public double FactureValue { get; set; }
+        public string ContractorFirstName { get; set; }
+        public string ContractorLastName { get; set; }
+        public string ContractorAddress { get; set; }
+        public string ContractorCompanyName { get; set; }
+        public string ContractorNIP { get; set; }
+        public List<PositionDto> Positions { get; set; }
+    }
+}
---
+++ CSupporter.Gateway/Dtos/PositionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CSupporter.Gateway.Dtos
+{
+    public class PositionDto
+    {
+        public int PositionNo { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public int ProductAmount { get; set; }
+    }
+}
